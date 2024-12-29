59e0d5e2bc673d3be923f445944fc22c3a1167f5
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-29 15:55:57 +0700

    mengubah nama

--- app/src/main/java/com/example/p12/ui/customwidget/CostumeTopAppBar.kt
+++ app/src/main/java/com/example/p12/ui/customwidget/CostumeTopAppBar.kt
@@ -15,7 +15,7 @@ import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CustomTopAppBar(
+fun CostumeTopAppBar(
   title: String,
   canNavigateBack: Boolean,
   modifier: Modifier = Modifier,
