a8a2b221d5fe23b70c272086c52c79dfdfbd7f3b
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-29 15:56:10 +0700

    membuat fungsi entrybody

---
+++ app/src/main/java/com/example/p12/ui/view/InsertView.kt
@@ -0,0 +1,57 @@
+package com.example.p12.ui.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.p12.ui.customwidget.CostumeTopAppBar
+import com.example.p12.ui.navigation.DestinasiNavigasi
+import com.example.p12.ui.viewmodel.InsertUiEvent
+import com.example.p12.ui.viewmodel.InsertUiState
+import com.example.p12.ui.viewmodel.InsertViewModel
+
+object DestinasiEntry : DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryMhsScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+//    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryBody()
+
+    }
+}
+
+@Composable
+fun EntryBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}
