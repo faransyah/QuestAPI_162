b9dac4a3f2bdb8bdd3a4444e5f944b8e1d9279a5
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-30 12:14:38 +0700

    menambahkan initializer untuk detail dan update

--- app/src/main/java/com/example/p12/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/p12/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.p12.ui.viewmodel
 
+import DetailMhsViewModel
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -10,6 +13,8 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiKontak().container.kontakRepository) }
         initializer { InsertViewModel(aplikasiKontak().container.kontakRepository) }
+        initializer { DetailMhsViewModel(createSavedStateHandle(),aplikasiKontak().container.kontakRepository) }
+        initializer { UpdateViewModel(createSavedStateHandle(),aplikasiKontak().container.kontakRepository) }
     }
 
 
