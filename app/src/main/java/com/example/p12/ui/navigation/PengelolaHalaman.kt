4b8f81698a9c520c4005cf5924da8a5bacf47c35
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-30 08:52:09 +0700

    membuat fungsi PengelolaHalaman

---
+++ app/src/main/java/com/example/p12/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,43 @@
+package com.example.p12.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.p12.ui.view.DestinasiEntry
+import com.example.p12.ui.view.DestinasiHome
+import com.example.p12.ui.view.EntryMhsScreen
+import com.example.p12.ui.view.HomeScreen
+import java.security.KeyStore.Entry
+
+@Composable
+fun PengelolaHalaman( modifier: Modifier = Modifier,
+                      navController: NavHostController = rememberNavController()){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClick = {}
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntryMhsScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(DestinasiHome.route) {
+                            inclusive = true
+                        }
+                    }
+                }
+            )
+        }
+
+    }
+
+}
